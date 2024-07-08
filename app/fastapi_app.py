3562cd29c78d53d30356b08234d36be4329034ce
Lolaperez <lolaperez1331@gmail.com>
2024-07-08 10:58:13 +0300

    prometeus metrics added

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from api_v1 import router as api_v1_router
 
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 app.include_router(api_v1_router, prefix="/api/v1")
+Instrumentator().instrument(app).expose(app)
